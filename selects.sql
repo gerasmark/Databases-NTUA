a304b77e7efae48734e99f7da476591bb8ce962a
gerasmark <gerasmark@hotmail.gr>
2022-06-02 16:07:53 +0300

    commit

--- selects.sql
+++ selects.sql
@@ -1,3 +1,8 @@
-SELECT f.name, f.title FROM fieldthatdescribes f
+--3.3
+SELECT f.name, f.title, r.first_name, r.last_name  FROM fieldthatdescribes f
 INNER JOIN project p ON p.title = f.title
-WHERE p.end_date > '2022-06-06';
+INNER JOIN researcher r ON p.evaluated_from = r.id
+WHERE r.works_since > '2021-06-06'
+AND p.end_date > '2022-06-06';
+
+--3.4
