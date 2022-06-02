2ceb6c48336360bab8fd4f35e159b72fbcd9f099
gerasmark <gerasmark@hotmail.gr>
2022-06-02 17:38:18 +0300

    commit

--- selects.sql
+++ selects.sql
@@ -11,3 +11,10 @@ SELECT o1.name as organization_1,
        o2.name as organization_2
  FROM organization o1
 INNER JOIN project p1 ON p1.from_org = o1.name 
+INNER JOIN project p2 ON COUNT(p1.from_org) = COUNT(p2.from_org)
+INNER JOIN organization o2 ON p2.from_org = o2.name 
+WHERE p1.start_date < (p2.start_date + '2-01-01')
+AND p1.start_date > (p2.start_date - '2-01-01')
+AND COUNT(p1.from_org) >= 20
+AND COUNT(p2.from_org) >= 20
+
