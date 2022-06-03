d84aaa399202c0e390681baa3b054d6990e4fe75
gerasmark <gerasmark@hotmail.gr>
2022-06-03 13:44:58 +0300

    commit

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `elide`.`program`;
 
 CREATE TABLE `elide`.`program` (
   `name` VARCHAR(45) NOT NULL,
-  `adress` VARCHAR(45) NULL,
+  `address` VARCHAR(45) NULL,
   PRIMARY KEY (`name`)
   )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
   
