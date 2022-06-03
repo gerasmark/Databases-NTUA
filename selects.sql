a4aa635dfa1759dcb6da608aaac343caee1f671e
KonstantinosKatsigiannis <105919276+KonstantinosKatsigiannis@users.noreply.github.com>
2022-06-04 02:54:07 +0300

    Update selects.sql

--- selects.sql
+++ selects.sql
@@ -69,13 +69,13 @@ LIMIT 3;
 DROP VIEW organizations_with_same_project_numbers;
 
 --3.6 check
-SELECT r.first_name, r.last_name, COUNT(*) as number_of_projects
+SELECT concat(r.first_name," ", r.last_name) as Full_name, COUNT(*) as number_of_projects
 FROM researcher r
 INNER JOIN worksfor w ON r.id = w.id
 INNER JOIN project p ON w.title = p.title
-WHERE r.age < 40 
+WHERE r.age < 40
 AND p.end_date > curdate() AND p.start_date < curdate()
-GROUP BY r.last_name, r.first_name
+GROUP BY Full_name
 ORDER BY number_of_projects DESC
 
 --3.7 check
