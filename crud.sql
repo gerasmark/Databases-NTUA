510b3fc001a93090f04c128632068eb8a40ef2ea
gerasmark <gerasmark@hotmail.gr>
2022-06-04 17:19:15 +0300

    commit

--- crud.sql
+++ crud.sql
@@ -1,7 +1,11 @@
 --read
-select * from program
+select * from '{}'
 
 --create
+INSERT INTO program (name,address) VALUES ('{}','{}')
+
+INSERT INTO organization (name,initials,postal_code,street,city) VALUES ('{}','{}','{}','{}')
+
 
 
 --update
