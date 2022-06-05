7dc8ba5fda54081195e316ea5089f1441d4fa693
gerasmark <gerasmark@hotmail.gr>
2022-06-05 17:54:36 +0300

    commit

--- test/app.py
+++ test/app.py
@@ -487,7 +487,7 @@ def create_entry():
             """.format(uname, ubudget_from_edu)
             cur1.execute(uqueryString)
             db.connection.commit()
-    
+
         if (rcname != '' and rcbudget_from_edu != '' and rcbudget_from_priv != ''):
             rcqueryString = """
             INSERT INTO research_center (name,budget_from_edu,budget_from_priv) VALUES ('{}','{}','{}');
@@ -495,21 +495,21 @@ def create_entry():
             cur1.execute(rcqueryString)
             db.connection.commit()
             cur1.close
-    
+
         if (dtitle != '' and dsummary != '' and dtitle_project != '' and ddue_date != ''):
             dqueryString = """
             INSERT INTO deliverable (title,summary,title_project,due_date) VALUES ('{}','{}','{}','{}');
             """.format(dtitle, dsummary, dtitle_project, ddue_date)
             cur1.execute(dqueryString)
             db.connection.commit()
-    
+
         if (sname != '' ):
             squeryString = """
             INSERT INTO scientific_field (name) VALUES ('{}');
             """.format(sname)
             cur1.execute(squeryString)
             db.connection.commit()
-    
+
         if (fname != '' and ftitle != '' ):
             fqueryString = """
             INSERT INTO fieldthatdescribes (name,title) VALUES ('{}','{}');
@@ -517,7 +517,12 @@ def create_entry():
             cur1.execute(fqueryString)
             db.connection.commit()
 
-
+        if (wtitle != '' and wid != '' ):
+            wqueryString = """
+            worksfor (title,id) VALUES ('{}','{}');
+            """.format(fname, ftitle)
+            cur1.execute(wqueryString)
+            db.connection.commit()
     return render_template('create_entry.html', queryString=queryString, errorprogram=errorprogram)
 
 @app.route("/update_entry")
