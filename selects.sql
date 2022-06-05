44308f19fd4168e2518a3b0227aa474da86feb8d
KonstantinosKatsigiannis <105919276+KonstantinosKatsigiannis@users.noreply.github.com>
2022-06-05 05:20:56 +0300

    a

--- selects.sql
+++ selects.sql
@@ -1,4 +1,4 @@
---3.1 check
+--3.1
 SELECT name FROM program;
 
 select distinct duration from project
@@ -9,91 +9,66 @@ order by exec
 
 SELECT p.title as Project_title
 from project p
-where p.exec = '{}' and p.duration = '{}' and ((p.start_date > '{}')  and (p.end_date < '{}'));
+where p.start_date < current_date()
+and p.exec = '{}' and p.duration = '{}' and ((p.start_date > '{}')  and (p.end_date < '{}'));
 
-select Full_name from 
-(select  p.title , concat(r.last_name," ", r.first_name) as Full_name
-from researcher r 
+select t.Full_name from
+(select p.title , concat(r.last_name," ", r.first_name) as Full_name
+from researcher r
 inner join worksfor w on r.id = w.id
 inner join project p on w.title = p.title
-order by Full_name) A
-where A.title = '%';
+where p.title = '{}'
+order by Full_name) t
+ORDER BY t.Full_name;
 
-select concat(r.last_name," ", r.first_name) as Full_name
-from researcher r 
-inner join worksfor w on r.id = w.id
-inner join project p on w.title = p.title
-order by Full_name
+--3.2
+
+SELECT concat(r.last_name," ", r.first_name) as Full_name, p.title as title_of_project
+FROM researcher r
+INNER JOIN worksfor w ON r.id = w.id
+INNER JOIN project p ON w.title = p.title
+ORDER BY Full_name;
 
 
---3.2 check 
+SELECT o.name as organization_name, p.title as title_of_project
+FROM organization o
+INNER JOIN project p on o.name = p.from_org
+ORDER BY organization_name;
 
-create view projects_per_researcher as
-(select concat(r.last_name," ", r.first_name) as Full_name, p.title as title_of_project 
-from researcher r 
-inner join worksfor w on r.id = w.id 
-inner join project p on w.title = p.title)
-order by Full_name ;
 
-select * from projects_per_researcher;
-
-create view projects_per_organization as 
-select o.name as organization_name, p.title as title_of_project
-from organization o 
-inner join project p on o.name = p.from_org
-order by organization_name ;
-
-select * from projects_per_organization;
-
-drop view projects_per_researcher;
-drop view projects_per_organization;
-
---3.3 check 
+--3.3
 select * from scientific_field;
 
 SELECT p.title, concat(r.last_name," ", r.first_name) as Full_name  FROM fieldthatdescribes f
 INNER JOIN project p ON f.title = p.title
 INNER JOIN worksfor w ON p.title = w.title
 INNER JOIN researcher r ON w.id = r.id
-WHERE r.works_since <= current_date() AND f.name = "Mathematics"
+WHERE r.works_since <= current_date() AND f.name = '{}'
 AND p.end_date > current_date() AND p.start_date < current_date()
 union
 SELECT p.title, p.exec as Full_name  FROM fieldthatdescribes f
 INNER JOIN project p ON f.title = p.title
-WHERE f.name = "Mathematics"
-AND p.end_date > current_date() AND p.start_date < current_date() 
+WHERE f.name = '{}'
+AND p.end_date > current_date() AND p.start_date < current_date()
 ORDER BY title;
 
---3.4 
-create view organizations as
-select o.name, extract( year from p.start_date) as year, count(*) as projects 
-from organization o 
-inner join project p on o.name = p.from_org
-group by year, name;
-select * from organizations;
+--3.4
 
 select o1.name, o1.year, o2.year,o1.projects
-from organizations o1
-inner join organizations o2 on o1.name = o2.name
-where ( o1.year <> o2.year and o1.year < o2.year)
-and o2.year - o1.year = 1 and o1.projects = o2.projects and o1.projects >= 10;
-
-select o1.name, o1.year, o2.year,o1.projects
-from (select o.name, extract( year from p.start_date) as year, count(*) as projects 
-from organization o 
+from (select o.name, extract( year from p.start_date) as year, count(*) as projects
+from organization o
 inner join project p on o.name = p.from_org
-group by year, name) o1 
-inner join (select o.name, extract( year from p.start_date) as year, count(*) as projects 
-from organization o 
+group by year, name) o1
+inner join (select o.name, extract( year from p.start_date) as year, count(*) as projects
+from organization o
 inner join project p on o.name = p.from_org
 group by year, name) o2 on o1.name = o2.name
 where ( o1.year <> o2.year and o1.year < o2.year)
 and o2.year - o1.year = 1 and o1.projects = o2.projects and o1.projects >= 10;
 
---3.5 check
-CREATE VIEW organizations_with_same_project_numbers as
+--3.5
 SELECT s1.name as project_1, s2.name as project_2
-FROM scientific_field s1 
+FROM scientific_field s1
 INNER JOIN fieldthatdescribes f1 ON s1.name = f1.name
 INNER JOIN fieldthatdescribes f2 ON f1.title = f2.title
 INNER JOIN scientific_field s2 ON f2.name = s2.name
@@ -101,9 +76,8 @@ WHERE s1.name <> s2.name
 GROUP BY s1.name, s2.name
 LIMIT 3;
 
-DROP VIEW organizations_with_same_project_numbers;
 
---3.6 check
+--3.6
 SELECT concat(r.first_name," ", r.last_name) as Full_name, COUNT(*) as number_of_projects
 FROM researcher r
 INNER JOIN worksfor w ON r.id = w.id
@@ -113,20 +87,20 @@ AND p.end_date > curdate() AND p.start_date < curdate()
 GROUP BY Full_name
 ORDER BY number_of_projects DESC
 
---3.7 check
+--3.7
 SELECT p.exec as project_executive, c.name as company_name, SUM(p.amount) as total_amount
-FROM project p 
+FROM project p
 INNER JOIN organization o ON p.from_org = o.name
 INNER JOIN company c ON o.name = c.name
 GROUP BY p.exec, o.name
 ORDER BY SUM(p.amount) DESC
 LIMIT 5;
 
---3.8 check 
+--3.8
 select * from (
 select concat(last_name, " ", first_name) as Full_name, count(*) as projects  from (
 (select r.last_name, r.first_name
-from researcher r 
+from researcher r
 inner join worksfor w on r.id = w.id
 inner join project p on w.title = p.title
 left join deliverable d on p.title = d.title_project
