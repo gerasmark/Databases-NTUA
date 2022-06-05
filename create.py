be8dd5089fdc4da535eb51b1e0c090fe3d9b0b16
gerasmark <gerasmark@hotmail.gr>
2022-06-05 16:20:20 +0300

    commit

---
+++ create.py
@@ -0,0 +1,38 @@
+def create_entry():
+    queryString = ''
+    oqueryString = ''
+    rqueryString = ''
+    rcqueryString = ''
+    uqueryString = ''
+    cqueryString = ''
+    pqueryString = ''
+    squeryString = ''
+    queryString = ''
+    if request.method == 'POST':
+        cur1 = db.connection.cursor()
+        pname = str(request.form.get('inputname'))
+        paddress = str(request.form.get('inputaddress'))
+
+        oname = str(request.form.get('inputoname'))
+        oinitials = str(request.form.get('inputoinitials'))
+        opostal_code = str(request.form.get('inputopostal_code'))
+        ostreet = str(request.form.get('inputostreet'))
+        ocity = str(request.form.get('inputocity'))
+
+
+        if (pname != 'None' and paddress != 'None'):
+             queryString = """
+             INSERT INTO program (name,address) VALUES ('{}','{}');
+             """.format(pname, paddress)
+        cur1.execute(queryString)
+        db.connection.commit()
+        cur1.close
+
+
+
+
+
+
+
+
+    return render_template('create_entry.html', queryString=queryString)
