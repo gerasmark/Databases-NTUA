955088730817eb012ddeedb8e91a205a356c580d
gerasmark <gerasmark@hotmail.gr>
2022-06-05 23:12:28 +0300

    commit

--- test/app.py
+++ test/app.py
@@ -1190,7 +1190,7 @@ def update_entry():
             cur.execute(queryStringInitial)
             ptitles = cur.fetchall()
             cur.close()
-
+        
         return render_template('update_entry.html',fqueryString=fqueryString, cnames=cnames, dtitle_projects=dtitle_projects, fnames=fnames, ftitles=ftitles, pfrom_orgs=pfrom_orgs, pevaluated_froms=pevaluated_froms, pnames=pnames, rnames=rnames, wids=wids, wtitles=wtitles, unames=unames, rcnames=rcnames, phnames=phnames, prnames = prnames, onames = onames, rids = rids, ptitles = ptitles)
     except:
         errorwhole = "Invalid update command"
