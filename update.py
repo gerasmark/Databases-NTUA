c9c5c8d1544669ddba63fb5dce9467163454b8df
gerasmark <gerasmark@hotmail.gr>
2022-06-05 19:20:37 +0300

    commit

--- update.py
+++ update.py
@@ -107,7 +107,7 @@
         if (phname != '' and phphone != '' ):
              phqueryString = """
              UPDATE phone SET phone = '{}' WHERE name = '{}';
-             """.format(phphone,phname)
+             """.format(phname,phphone)
              cur1.execute(phqueryString)
              db.connection.commit()
         else:
